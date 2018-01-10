9727fcdbecb21e2e6c7320ee731c870ecd7215f3
ComBarnea <com.barnea@gmail.com>
2018-01-10 17:11:27 +0200

    :sparkles: :house: new build

---
+++ lib/index.d.ts
@@ -0,0 +1,17 @@
+import { ICacheProvider, ICacheProviderOptions, CacheProvider } from 'sarah.js';
+/**
+ * Controll caches lifetime.
+ */
+export declare class MemoryProvider extends CacheProvider implements ICacheProvider {
+    private caches;
+    name: string;
+    constructor(options?: ICacheProviderOptions);
+    private compare(singleHash);
+    set(requestedData: {
+        val: any;
+        hash: string;
+    }[], ttl?: number): Promise<any | any[]>;
+    get(requestedHashes: string[]): Promise<any | any[]>;
+    invalidateCache(requestedHash: any, ttl: number): Promise<void>;
+    private removeFromCache(requestedHash);
+}
